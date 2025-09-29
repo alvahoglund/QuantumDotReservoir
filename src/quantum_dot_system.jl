f2219084708683813d1f36e22c9b652233357efc
alvahoglund <hoglundalva@gmail.com>
2025-09-29 10:20:37 +0200

    Fix how coordinates at intersection are set

--- src/quantum_dot_system.jl
+++ src/quantum_dot_system.jl
@@ -23,10 +23,10 @@ function tight_binding_system(nbr_dots_main, nbr_dots_res, qn_reservoir)
     coordinates_main, coordinates_reservoir, coordinates_total, coordinates_intersection = generate_grid(nbr_dots_main, nbr_dots_res)
     qn_total = qn_reservoir + nbr_dots_main
 
-    H_main_qn = tensor_product(hilbert_space(labels(sites_main[1]), NumberConservation(1)), hilbert_space(labels(sites_main[2]), NumberConservation(1)))
+    H_main_qn = tensor_product(hilbert_space(labels([coordinates_main[1]]), NumberConservation(1)), hilbert_space(labels([coordinates_main[2]]), NumberConservation(1)))
     H_main = hilbert_space(keys(H_main_qn), NumberConservation())
-    H_reservoir = hilbert_space(labels(sites_reservoir), NumberConservation())
-    H_total = hilbert_space(labels(vcat(sites_main, sites_reservoir)), NumberConservation())
+    H_reservoir = hilbert_space(labels(coordinates_reservoir), NumberConservation())
+    H_total = hilbert_space(labels(coordinates_total), NumberConservation())
 
     @fermions f
 
@@ -41,7 +41,7 @@ function generate_grid(nbr_dots_main::Int, nbr_dots_reservoir::Int)
         for i in 1:nbr_dots_reservoir 
     ]
     coordinates_total = vcat(coordinates_main, coordinates_reservoir)
-    coordinates_intersection = vcat(coordinates_main, [coordinate for coordinate in coordinates_reservoir if coordinate[1] ==2])
+    coordinates_intersection = vcat(coordinates_main, [coordinate for coordinate in coordinates_reservoir if coordinate[2] ==2])
     return coordinates_main, coordinates_reservoir, coordinates_total, coordinates_intersection
 end
 
