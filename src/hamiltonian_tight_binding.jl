39245bed743276ec8ef92f57b3110eaedc030bed
alvahoglund <hoglundalva@gmail.com>
2025-09-30 13:44:58 +0200

    Fix bugs in the tight binding hailtonian

--- src/hamiltonian_tight_binding.jl
+++ src/hamiltonian_tight_binding.jl
@@ -1,13 +1,13 @@
 struct DotParams
-    ϵ::Vector{Float64}
-    ϵb::Matrix{Float64}
-    u_intra::Vector{Float64}
+    ϵ
+    ϵb
+    u_intra
 end
 
 struct InteractionParams
-    t::Matrix{ComplexF64}
-    tso::Matrix{ComplexF64}
-    u_inter::Matrix{Float64}
+    t
+    t_so
+    u_inter
 end
 
 struct Hamiltonians
@@ -18,17 +18,25 @@ struct Hamiltonians
 end
 
 ## ============ Singel dot ================
+
 hamiltonian_ϵ(ϵ, coordinate_labels, f) = sum(
     ϵ[label]*f[label, σ]' * f[label, σ] 
-    for σ ∈ [:↑, :↓], (i,label) ∈ enumerate(coordinate_labels))
+    for σ ∈ [:↑, :↓], label ∈ coordinate_labels;
+    init=0
+)
 
 hamiltonian_b(ϵb, coordinate_labels, f) = sum(
     ϵb[label]*(-1)^(n+1)*f[label,σ]'f[label,σ]
-    for (n,σ) ∈ enumerate([:↑, :↓]), (i,label) ∈ enumerate(coordinate_labels))
+    for (n,σ) ∈ enumerate([:↑, :↓]), label ∈ coordinate_labels;
+    init=0
+)
+
 
 hamiltonian_c_intra(u_intra, coordinate_labels, f) = sum(
-    u_intra[i]*f[label,:↑]'f[label, :↓]'f[label, :↓]f[label, :↑]
-    for (i, label) ∈ enumerate(coordinate_labels))
+    u_intra[label]*f[label,:↑]'f[label, :↓]'f[label, :↓]f[label, :↑]
+    for label ∈ coordinate_labels;
+    init=0
+)
 
 ## ============ Interactions ================
 
@@ -36,33 +44,39 @@ hamiltonian_c_inter(u_inter, coordinate_labels, f) = hamiltonian_c_inter_x(u_int
 
 hamiltonian_c_inter_x(u_inter, coordinate_labels, f) = sum(
     u_inter[(i,j), (i+1,j)]*f[(i,j), σ1]'f[(i+1,j),σ2]'f[(i+1,j),σ2]f[(i,j),σ1]
-    for σ1 ∈ [:↑, :↓], σ2 ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i+1, j) ∈ coordinate_labels
+    for σ1 ∈ [:↑, :↓], σ2 ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i+1, j) ∈ coordinate_labels;
+    init=0
 )
 hamiltonian_c_inter_y(u_inter, coordinate_labels, f) = sum(
     u_inter[(i,j), (i,j+1)]*f[(i,j), σ1]'f[(i,j+1),σ2]'f[(i,j+1),σ2]f[(i,j),σ1]
-    for σ1 ∈ [:↑, :↓], σ2 ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i, j+1) ∈ coordinate_labels
+    for σ1 ∈ [:↑, :↓], σ2 ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i, j+1) ∈ coordinate_labels;
+    init=0
 )
 
 hamiltonian_t(t, coordinate_labels, f) = hamiltonian_t_x(t, coordinate_labels, f) + hamiltonian_t_y(t, coordinate_labels, f)
 
 hamiltonian_t_x(t, coordinate_labels, f) = sum(
     t[(i,j), (i+1, j)]f[(i+1,j), σ]'f[(i, j),σ] + hc
-    for σ ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i+1, j) ∈ coordinate_labels
+    for σ ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i+1, j) ∈ coordinate_labels;
+        init=0
 )
 hamiltonian_t_y(t, coordinate_labels, f) = sum(
     t[(i,j), (i, j+1)]f[(i,j+1), σ]'f[(i, j),σ] + hc
-    for σ ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i, j+1) ∈ coordinate_labels
+    for σ ∈ [:↑, :↓], (i,j) ∈ coordinate_labels if (i, j+1) ∈ coordinate_labels;
+    init=0
 )
 
 hamiltonian_so(t_so, coordinate_labels, f) = hamiltonian_so_x(t_so, coordinate_labels, f) + hamiltonian_so_y(t_so, coordinate_labels, f)
 
 hamiltonian_so_x(t_so, coordinate_labels, f) = sum(
-    t_so[(i,j), (i+1, j)]*(-f[(i+1,j), ↑]'f[(i,j),↓] + f[(i+1,j), ↑]'f[(i,j),↓]) + hc
-    for (i,j) ∈ coordinate_labels if (i+1, j) ∈ coordinate_labels
+    t_so[(i,j), (i+1, j)]*(-f[(i+1,j), :↑]'f[(i,j),:↓] + f[(i+1,j), :↑]'f[(i,j),:↓]) + hc
+    for (i,j) ∈ coordinate_labels if (i+1, j) ∈ coordinate_labels;
+    init=0
 )
 hamiltonian_so_y(t_so, coordinate_labels, f) = sum(
-    t_so[(i,j), (i, j+j)]*(im*f[(i,j+1), ↑]'f[(i,j),↓] + im*f[(i,j+j), ↑]'f[(i,j),↓]) + hc
-    for (i,j) ∈ coordinate_labels if (i, j+1) ∈ coordinate_labels
+    t_so[(i,j), (i, j+j)]*(im*f[(i,j+1), :↑]'f[(i,j),:↓] + im*f[(i,j+j), :↑]'f[(i,j),:↓]) + hc
+    for (i,j) ∈ coordinate_labels if (i, j+1) ∈ coordinate_labels;
+    init=0
 )
 
 ## ========= Set Dot Parameters =============
@@ -82,9 +96,9 @@ function randomize_dot_param(coordinates)
 end
 function randomize_interaction_param(coordinates)
     coupled_coordinates = get_coupled_coordinates(coordinates)
-    t = (coupled_coordinate => rand() for coupled_coordinate in coupled_coordinates)
-    t_so = (coupled_coordinate => rand() for coupled_coordinate in coupled_coordinates)
-    u_inter = (coupled_coordinate => rand() for coupled_coordinate in coupled_coordinates)
+    t = Dict(coupled_coordinate => rand() for coupled_coordinate in coupled_coordinates)
+    t_so = Dict(coupled_coordinate => rand() for coupled_coordinate in coupled_coordinates)
+    u_inter = Dict(coupled_coordinate => rand() for coupled_coordinate in coupled_coordinates)
     InteractionParams(t,t_so,u_inter)
 end
 
@@ -105,13 +119,14 @@ hamiltonian_interactions(interaction_params, coordinates, f) =
 hamiltonian_intersection(interaction_params, coordinates, f) = 
     hamiltonian_t_x(interaction_params.t, coordinates, f) + hamiltonian_so_x(interaction_params.t_so, coordinates, f) + hamiltonian_c_inter_x(interaction_params.u_inter, coordinates, f) 
 
-function hamiltonian(quantum_dot_system, f)
-    dot_params = merge(main_system_dot_param(quantum_dot_system.coordinates_main), randomize_dot_param(quantum_dot_system.sites_reservoir))
-    interaction_params = randomize_interaction_param(quantum_dot_system.sites_total)
+function hamiltonians(quantum_dot_system)
+    dot_params_main = main_system_dot_param(quantum_dot_system.coordinates_main)
+    dot_params_reservoir = randomize_dot_param(quantum_dot_system.coordinates_reservoir)
+    interaction_params = randomize_interaction_param(quantum_dot_system.coordinates_total)
 
-    hamiltonian_main = hamiltonian_dots(dot_params, quantum_dot_system.sites_main, f) + hamiltonian_interactions(interaction_params, quantum_dot_system.sites_main, f)
-    hamiltonian_reservoir = hamiltonian_dots(dot_params, quantum_dot_system.sites_reservoir, f) + hamiltonian_interactions(interaction_params, quantum_dot_system.sites_reservoir, f)
-    hamiltonian_intersection = hamiltonian_interactions(interaction_params, quantum_dot_system.sites_intersection, f)
+    hamiltonian_main = hamiltonian_dots(dot_params_main, quantum_dot_system.coordinates_main, quantum_dot_system.f) + hamiltonian_interactions(interaction_params, quantum_dot_system.coordinates_main, quantum_dot_system.f)
+    hamiltonian_reservoir = hamiltonian_dots(dot_params_reservoir, quantum_dot_system.coordinates_reservoir, quantum_dot_system.f) + hamiltonian_interactions(interaction_params, quantum_dot_system.coordinates_reservoir, quantum_dot_system.f)
+    hamiltonian_intersection = hamiltonian_interactions(interaction_params, quantum_dot_system.coordinates_intersection, quantum_dot_system.f)
     hamiltonian_total = hamiltonian_main + hamiltonian_reservoir + hamiltonian_intersection 
     return hamiltonian_main, hamiltonian_reservoir, hamiltonian_intersection, hamiltonian_total
 end
