cfe76361b6dcd4082ca34343fd06bd31083d39a2
alvahoglund <hoglundalva@gmail.com>
2025-10-01 14:30:52 +0200

    Update project files

--- init.jl
+++ init.jl
@@ -1,12 +1,11 @@
 using Pkg
 Pkg.activate(pwd())
 Pkg.instantiate()
-using LinearAlgebra, FermionicHilbertSpaces, Revise, Random, Arpack, SparseArrays, Plots, LinearMaps, TestItems, Test
+using LinearAlgebra, FermionicHilbertSpaces, Revise, Random, Arpack, SparseArrays, Plots, LinearMaps, TestItems, Test, NonCommutativeProducts
 
 includet("src//quantum_dot_system.jl")
-includet("src//hilbert_space.jl")
-includet("src//hamiltonian.jl")
 includet("src//states.jl")
 includet("src//time_evolution.jl")
 includet("src//measurements.jl")
 includet("src//effective_measurements.jl")
+includet("src//hamiltonian_tight_binding.jl")
