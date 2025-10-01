84df3c7b01e589db5de5720da4c784f103e8bb88
alvahoglund <hoglundalva@gmail.com>
2025-10-01 14:26:02 +0200

    Remove old hamiltonian for fully connected system

--- src/quantum_dot_system.jl
+++ src/quantum_dot_system.jl
@@ -44,20 +44,3 @@ function generate_grid(nbr_dots_main::Int, nbr_dots_reservoir::Int)
     coordinates_intersection = vcat(coordinates_main, [coordinate for coordinate in coordinates_reservoir if coordinate[1] ==2])
     return coordinates_main, coordinates_reservoir, coordinates_total, coordinates_intersection
 end
-
-function fully_connected_system(nbr_dots_main, nbr_dots_res, qn_reservoir)
-    sites_main = collect(1:nbr_dots_main)
-    sites_reservoir = collect(nbr_dots_main+1:nbr_dots_main+nbr_dots_res)
-    sites_total = vcat(sites_main, sites_reservoir)
-    qn_total = qn_reservoir + nbr_dots_main
-
-    H_main_qn = tensor_product(hilbert_space(labels(sites_main[1]), NumberConservation(1)), hilbert_space(labels(sites_main[2]), NumberConservation(1)))
-    H_main = hilbert_space(keys(H_main_qn), NumberConservation())
-    H_reservoir = hilbert_space(labels(sites_reservoir), NumberConservation())
-    H_total = hilbert_space(labels(vcat(sites_main, sites_reservoir)), NumberConservation())
-
-    @fermions f
-
-    return QuantumDotSystem(sites_main, sites_reservoir, sites_total, sites_total, nbr_dots_main, qn_reservoir, qn_total,
-                            H_main_qn, H_main, H_reservoir, H_total, f)
-end
