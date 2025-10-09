97a444104e646ead584cd019f63a86cc69517f53
alvahoglund <hoglundalva@gmail.com>
2025-10-09 10:19:07 +0200

    Add test for effective measurements and time evolution

--- Tests/test_time_evolution.jl
+++ Tests/test_time_evolution.jl
@@ -16,26 +16,35 @@
 
     @test exp_value_op≈ exp_value_ρ
 end
+@testitem "Effective measurements & time evolution" begin
+    ## Initialize system
+    quantum_dot_system = tight_binding_system(2,2,1)
+    seed = 2
+    hams = hamiltonians(quantum_dot_system, seed)
+    reservoir_state = ground_state(hams.hamiltonian_reservoir, quantum_dot_system.H_reservoir, quantum_dot_system.qn_reservoir)
 
+    initial_states = [def_state(triplet_plus, quantum_dot_system.H_main, quantum_dot_system.f),
+                        def_state(singlet, quantum_dot_system.H_main, quantum_dot_system.f),
+                        random_product_state(quantum_dot_system),
+                        random_separable_state(3, quantum_dot_system)]
 
-@testitem "Effective measurements" begin
-    @fermions f
-    Random.seed!(2)
-    quantum_dot_system = tight_binding_system(2,3,1)
-    hams = hamiltonians(quantum_dot_system)
+    total_states = map(initial_state -> tensor_product((initial_state, reservoir_state), (quantum_dot_system.H_main, quantum_dot_system.H_reservoir)=> quantum_dot_system.H_total), initial_states)
+    measurements = map(op -> matrix_representation(op, quantum_dot_system.H_total), charge_measurements(quantum_dot_system))
 
-    ham1 = matrix_representation(hams.hamiltonian_main, quantum_dot_system.H_main)
-    ρ1 = ground_state(ham1)
-    ham2 = matrix_representation(hams.hamiltonian_reservoir, quantum_dot_system.H_reservoir)
-    ρ2 = ground_state(ham2)
+    t = 10
+    ham_total = matrix_representation(hams.hamiltonian_total,quantum_dot_system.H_total)
+
+    time_evolved_states = map(total_state -> state_time_evolution(total_state, t, ham_total, quantum_dot_system.H_total, quantum_dot_system.qn_total), total_states)
+    time_evolved_measurements = map(measurement -> operator_time_evolution(measurement, t, ham_total, quantum_dot_system.qn_total, quantum_dot_system.H_total), measurements)
+    effective_measurements = map(measurement -> effective_measurement(measurement, reservoir_state, quantum_dot_system), time_evolved_measurements)
+
+    ind = FermionicHilbertSpaces.indices(quantum_dot_system.H_main_qn, quantum_dot_system.H_main)
     
-    op = matrix_representation(nbr_op((1,1),quantum_dot_system.f), quantum_dot_system.H_total)
-    
-    ρ12 = tensor_product((ρ1, ρ2), (quantum_dot_system.H_main, quantum_dot_system.H_reservoir)=> quantum_dot_system.H_total)
-    exp_value = expectation_value(ρ12, op)
-
-    op_eff = effective_measurement(op, ρ2, quantum_dot_system.H_main, quantum_dot_system.H_reservoir, quantum_dot_system.H_total)
-    exp_value_eff = expectation_value(ρ1, op_eff)
-    @test exp_value ≈ exp_value_eff
+    for i in eachindex(initial_states), j in eachindex(measurements)
+        exp_val_1 = expectation_value(time_evolved_states[i], measurements[j])
+        exp_val_2 = expectation_value(total_states[i], time_evolved_measurements[j]) 
+        exp_val_3 = expectation_value(initial_states[i][ind,ind], effective_measurements[j])
+        @test exp_val_1 ≈ exp_val_2 ≈ exp_val_3
+    end
 end
 
