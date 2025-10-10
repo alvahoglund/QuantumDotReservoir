6ba8d85aa9d4b875171ebd7553d7bf522ef9a3a6
alvahoglund <hoglundalva@gmail.com>
2025-10-10 10:31:52 +0200

    Update examples

--- Examples/scrambling_map.jl
+++ Examples/scrambling_map.jl
@@ -1,10 +1,8 @@
 ##############
 nbr_dots_main = 2
-nbr_dots_res = 6
+nbr_dots_res = 4
 qn_reservoir = 0
 qd_system = tight_binding_system(nbr_dots_main, nbr_dots_res, qn_reservoir)
-
-quantum_dot_system = tight_binding_system(2,6,0)
 seed = 2
 hams = hamiltonians(quantum_dot_system, seed)
 reservoir_state = ground_state(hams.hamiltonian_reservoir, quantum_dot_system.H_reservoir, quantum_dot_system.qn_reservoir)
@@ -23,26 +21,9 @@ ham_total = matrix_representation(hams.hamiltonian_total,quantum_dot_system.H_to
 time_evolved_states = map(total_state -> state_time_evolution(total_state, t, ham_total, quantum_dot_system.H_total, quantum_dot_system.qn_total), total_states)
 time_evolved_measurements = map(measurement -> operator_time_evolution(measurement, t, ham_total, quantum_dot_system.qn_total, quantum_dot_system.H_total), measurements)
 effective_measurements = map(measurement -> effective_measurement(measurement, reservoir_state, quantum_dot_system), time_evolved_measurements)
-sm = scrambling_map(qd_system, measurements, ρ_res, matrix_representation(hams.hamiltonian_total, qd_system.H_total), t)
+sm = scrambling_map(quantum_dot_system, measurements, reservoir_state, matrix_representation(hams.hamiltonian_total, quantum_dot_system.H_total), t)
 
-Matrix(effective_measurements[1])
-sm
+measured_values = map(m -> expectation_value(time_evolved_states[3], m), measurements)
+reshape(inv(sm)*measured_values, 4,4) ≈ initial_states[3][ind,ind]
 
-
-scrambling_map_temp = vcat([vec(m) for m in effective_measurements]...)
-
-expectation_value(initial_states[3][ind, ind], effective_measurements[1]) 
-transpose(sm[1, :]) * vec(initial_states[3][ind,ind])
-
-
-(sm * vec(initial_states[3][ind,ind]))[1]
-
-###############################################
-
-prop = propagator(t, ham_tot, qd_system.qn_total, qd_system.H_total)
-
-process_measurements = op -> effective_measurement(
-    operator_time_evolution(sparse(prop), sparse(op)), ρ_res, qd_system
-) 
-eff_measurements = map(process_measurements, measurements)
-scrambling_map_temp2 = vcat([vec(m) for m in eff_measurements]...)
+###############
