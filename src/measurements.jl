e4f0a406845c46463dc4ab78e4a27f56d774cee1
alvahoglund <hoglundalva@gmail.com>
2025-10-23 16:58:53 +0200

    Add operators for charge probabilities and add option for measurements in scrambling map

--- src/measurements.jl
+++ src/measurements.jl
@@ -7,6 +7,11 @@
 nbr_op(coordinate, f) = f[coordinate, :↑]'*f[coordinate, :↑] + f[coordinate, :↓]'*f[coordinate, :↓]
 nbr2_op(coordinate, f) = f[coordinate, :↑]'*f[coordinate, :↑]*f[coordinate, :↓]'*f[coordinate, :↓]
 
+p(nbr_index, coordinate, f) = eval(Expr(:call, Symbol("p", nbr_index), coordinate, f)) 
+p0(coordinate, f) = 1 - p1(coordinate, f) - p2(coordinate,f) #Probability to measure 0 charge
+p1(coordinate, f) = nbr_op(coordinate, f) - 2*nbr2_op(coordinate,f) # Probability to measure 1 charge
+p2(coordinate, f) = nbr2_op(coordinate, f) # Probability to measure 2 charges
+
 pauli_string(σi, σj, qd_system) = tensor_product((matrix_representation(σi(qd_system.coordinates_main[1], qd_system.f), qd_system.H_main_a), 
                 matrix_representation(σj(qd_system.coordinates_main[2], qd_system.f), qd_system.H_main_b)),
                 (qd_system.H_main_a, qd_system.H_main_b) => qd_system.H_main)
@@ -14,6 +19,7 @@ pauli_string(σi, σj, qd_system) = tensor_product((matrix_representation(σi(qd
 
 process_complex(value, tolerance=1e-3) = abs(imag(value)) < tolerance ? real(value) : throw(ArgumentError("The value has an imaginary part: $(imag(value))"))
 expectation_value(ρ, op) = process_complex((tr(ρ * op)))
+variance(ρ, op) =expectation_value(ρ, op^2) - expectation_value(ρ, op)^2
 
 ## ======== Measurement sets =================
 
@@ -22,3 +28,7 @@ pauli_strings(qd_system) = [pauli_string(σi, σj, qd_system) for σi in [σ0, �
 single_charge_measurements(qd_system) = [nbr_op(coordinate, qd_system.f) for coordinate in qd_system.coordinates_total]
 double_charge_measurements(qd_system) = [nbr2_op(coordinate, qd_system.f) for coordinate in qd_system.coordinates_total]
 charge_measurements(qd_system) = vcat(single_charge_measurements(qd_system), double_charge_measurements(qd_system))
+
+single_charge_probabilities(qd_system) = [p1(coordinate, qd_system.f) for coordinate in qd_system.coordinates_total]
+double_charge_probabilities(qd_system) = [p2(coordinate, qd_system.f) for coordinate in qd_system.coordinates_total]
+charge_probabilities(qd_system) = vcat(single_charge_probabilities(qd_system), double_charge_probabilities(qd_system))
--- src/scrambling_map.jl
+++ src/scrambling_map.jl
@@ -9,23 +9,26 @@ function scrambling_map(qd_system :: QuantumDotSystem, measurements, ρ_res, ham
     return scrambling_map
 end
 
+scrambling_map(qd_system, t::Number, seed = nothing) = scrambling_map(qd_system, t, charge_measurements, seed) 
+scrambling_map(qd_system, t_list::AbstractArray, seed = nothing) = scrambling_map(qd_system, t_list, charge_measurements, seed) 
 
-function scrambling_map(qd_system, t::Number, seed = nothing)    
+function scrambling_map(qd_system, t::Number, measurement_types, seed = nothing)    
     isnothing(seed) ? hams = hamiltonians(qd_system) : hams = hamiltonians(qd_system, seed)
     hamiltonian_total = matrix_representation(hams.hamiltonian_total, qd_system. H_total)
     ρ_res = ground_state(hams.hamiltonian_reservoir, qd_system.H_reservoir, qd_system.qn_reservoir)
-    measurements = map(op -> matrix_representation(op, qd_system.H_total), charge_measurements(qd_system))
+    measurements = map(op -> matrix_representation(op, qd_system.H_total), measurement_types(qd_system))
     return scrambling_map(qd_system, measurements, ρ_res, hamiltonian_total, t)
 end
 
-function scrambling_map(qd_system, t_list :: AbstractArray, seed = nothing)
+function scrambling_map(qd_system, t_list :: AbstractArray, measurement_types, seed = nothing)
     isnothing(seed) ? hams = hamiltonians(qd_system) : hams = hamiltonians(qd_system, seed)
     hamiltonian_total = matrix_representation(hams.hamiltonian_total, qd_system. H_total)
     ρ_res = ground_state(hams.hamiltonian_reservoir, qd_system.H_reservoir, qd_system.qn_reservoir)
-    measurements = map(op -> matrix_representation(op, qd_system.H_total), charge_measurements(qd_system))
+    measurements = map(op -> matrix_representation(op, qd_system.H_total), measurement_types(qd_system))
     scrambling_maps = [scrambling_map(qd_system, measurements, ρ_res, hamiltonian_total, t) for t in t_list]
     return vcat(scrambling_maps...)
 end
+
 
 function pauli_string_map(qd_system)
     measurements = pauli_strings(qd_system)
