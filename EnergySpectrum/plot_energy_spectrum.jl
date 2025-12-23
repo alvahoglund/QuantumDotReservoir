83464b33fab41d36eff8928c2fc8e9a480f8c964
alvahoglund <hoglundalva@gmail.com>
2025-12-23 11:03:43 +0100

    Add heatmap of probabilitie and coheences

--- EnergySpectrum/plot_energy_spectrum.jl
+++ EnergySpectrum/plot_energy_spectrum.jl
@@ -1,7 +1,7 @@
 function get_H_Ham(nbr_dots_res)
     nbr_dots_main, nbr_dots_res, qn_reservoir = 2, nbr_dots_res, 0
     qd_system = tight_binding_system(nbr_dots_main, nbr_dots_res, qn_reservoir)
-    hams = hamiltonians(qd_system)
+    hams = hamiltonians_equal_param(qd_system)
     ham_tot= matrix_representation(hams.hamiltonian_total, qd_system.H_total)
     return hams, qd_system
 end
@@ -21,7 +21,7 @@ end
 
 function get_state_energy(qn_res, hams, qd_system)
     qd_system_qn = tight_binding_system(2, length(qd_system.coordinates_reservoir), qn_res)
-    ρ_main = def_state(singlet, qd_system_qn.H_main, qd_system_qn.f)
+    ρ_main = def_state(triplet_0, qd_system_qn.H_main, qd_system_qn.f)
     ρ_reservoir = ground_state(hams.hamiltonian_reservoir, qd_system_qn.H_reservoir, qn_res)
     ρ_total = tensor_product((ρ_main, ρ_reservoir), (qd_system_qn.H_main, qd_system_qn.H_reservoir) => qd_system_qn.H_total)
     ham_tot = matrix_representation(hams.hamiltonian_total, qd_system.H_total)
@@ -34,7 +34,9 @@ function get_state_energy(qn_res, hams, qd_system)
     return ρ_tot_qn, E_exp
 end
 
-energy_eigenbasis(vecs, ρ_tot_qn) = [real(vecs[:, i]' * ρ_tot_qn * vecs[:, i]) for i in 1:size(vecs)[1]]
+energy_amplitudes(vecs, ρ_tot_qn) = [real(vecs[:, i]' * ρ_tot_qn * vecs[:, i]) for i in 1:size(vecs)[1]]
+energy_amplitudes_matrix(vecs, ρ_tot_qn) = vecs' * ρ_tot_qn * vecs
+
 
 function get_plot_index(nbr_dots_res, qn_res)
     if qn_res ≤ nbr_dots_res
@@ -50,7 +52,7 @@ end
 function plot_data!(p_es, subplot_idx, vals, E_exp, c_i2, qn_res)
         xvals = 1:length(vals)
         ymin, ymax = minimum(vals)-0.1, maximum(vals)+0.1
-        background = repeat(reshape(c_i2, 1, :), 100, 1)
+        background = repeat(reshape(c_i2[xvals], 1, :), 100, 1)
 
         # Background heatmap in the subplot
         heatmap!(p_es, xvals, LinRange(ymin, ymax, 100), background,
@@ -61,7 +63,7 @@ function plot_data!(p_es, subplot_idx, vals, E_exp, c_i2, qn_res)
                  subplot=subplot_idx)
 
         # Overlay scatter points
-        scatter!(p_es, xvals, vals, color=:white, ms=5,
+        scatter!(p_es, xvals, vals[xvals], color=:white, ms=5,
                  title="Electrons in reservoir: $(qn_res)",
                  subplot=subplot_idx)
 
@@ -75,7 +77,7 @@ end
 
 function plot_spectrum(nbr_dots_res)
     hams, qd_system = get_H_Ham(nbr_dots_res)
-    p_es = plot(layout = (nbr_dots_res + 1, 2), size = (1500, 1500))
+    p_es = plot(layout = (nbr_dots_res + 1, 2), size = (1100, 1100))
 
     for qn_res in 0:2*nbr_dots_res
         row, col = get_plot_index(nbr_dots_res, qn_res)
@@ -83,7 +85,7 @@ function plot_spectrum(nbr_dots_res)
 
         vals, vecs = get_energy_spectrum(qn_res, hams, qd_system)
         ρ_tot_qn, E_exp = get_state_energy(qn_res, hams, qd_system)
-        c_i2 = energy_eigenbasis(vecs, ρ_tot_qn)
+        c_i2 = energy_amplitudes(vecs, ρ_tot_qn)
 
         plot_data!(p_es, subplot_idx, vals, E_exp, c_i2, qn_res)
     end
@@ -94,4 +96,15 @@ function plot_spectrum(nbr_dots_res)
     display(p_es)
 end
 
-plot_spectrum(3)
+function plot_energy_amplitudes_heatmap(nbr_dots_res, qn_res)
+    hams, qd_system = get_H_Ham(nbr_dots_res)
+    vals, vecs = get_energy_spectrum(qn_res, hams, qd_system)
+    ρ_tot_qn, E_exp = get_state_energy(qn_res, hams, qd_system)
+    c_ij = energy_amplitudes_matrix(vecs, ρ_tot_qn)
+
+    p_heatmap = plot(title = "Probabilities and coherences of energy eigenstates", xlabel = "Index", ylabel = "Index")
+    heatmap!(p_heatmap, abs.(c_ij), color=cgrad([:white, :purple]))
+end
+
+plot_energy_amplitudes_heatmap(2,0)
+plot_spectrum(2)
