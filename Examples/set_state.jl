78b66291715e875037a0ad84bcecd4ce559925b0
alvahoglund <hoglundalva@gmail.com>
2026-01-22 09:59:09 +0100

    Update example of state timne evolution

--- Examples/set_state.jl
+++ Examples/set_state.jl
@@ -6,7 +6,7 @@ qd_system = tight_binding_system(nbr_dots_main, nbr_dots_reservoir, qn_reservoir
 
 ## Hamiltonians
 seed = 5
-hams = hamiltonians(qd_system, seed)
+hams = hamiltonians_equal_param(qd_system)
 
 ## Main system state
 ρ_main = def_state(triplet_0, qd_system.H_main, qd_system.f)
@@ -27,30 +27,33 @@ hams.dot_params_reservoir.ϵ
 ham_total = matrix_representation(hams.hamiltonian_total, qd_system.H_total)
 
 ## Evolve state
-ρt_range = [state_time_evolution(ρ_total, t, ham_total) for t in range(0, 4*2π, 200)]
+t_range = range(0, 10*2π, 2000)
+ρt_range = [state_time_evolution(ρ_total, t, ham_total, qd_system.H_total, qd_system.qn_total) for t in t_range]
 
 ## Measure charge over time
 gr()
-p = plot()
+p_1 = plot()
 for i in qd_system.coordinates_total
     m = matrix_representation(nbr_op(i, qd_system.f), qd_system.H_total)
     y = [expectation_value(m, ρt) for ρt in ρt_range]
-    plot!(p, y, label="Site $i", title = "Charge expectation values in quantum dots") 
+    plot!(p_1, t_range, y, label="Site $i", title = "Charge expectation values in quantum dots") 
 end
-display(p)
+display(p_1)
 
 gr()
-p2 = plot()
+p_2 = plot()
 ms_main = [matrix_representation(nbr_op(coordinate, qd_system.f), qd_system.H_total) for coordinate in qd_system.coordinates_main]
 y2_main = [sum(expectation_value(m, ρt) for m in ms_main) for ρt in ρt_range]
-plot!(p2, y2_main, label="Main")
+plot!(p_2, t_range, y2_main, label="Main")
 ms_reservoir =[matrix_representation(nbr_op(coordinate, qd_system.f), qd_system.H_total) for coordinate in qd_system.coordinates_reservoir]
 y2_reservoir = y2_main = [sum(expectation_value(m, ρt) for m in ms_reservoir) for ρt in ρt_range]
-plot!(p2, y2_reservoir, label="Reservoir", title = "Total charge expectation values")
-display(p2)
+plot!(p_2, t_range, y2_reservoir, label="Reservoir", title = "Total charge expectation values")
+display(p_2)
 
 
 i_sub = FermionicHilbertSpaces.indices(qd_system.qn_reservoir, qd_system.H_reservoir)
 ham_res = matrix_representation(hams.hamiltonian_reservoir, qd_system.H_reservoir)
 vals, vecs = eigen(Matrix(ham_res))
 @show ham_res[i_sub, i_sub]
+
+tr(ρt_range[100]* total_spin_op(qd_system.coordinates_total, qd_system.f, qd_system.H_total))
