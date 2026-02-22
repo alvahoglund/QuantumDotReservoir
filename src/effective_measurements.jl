2d42c36eefd8e3236fbd01ee6156a421aacadddc
alvahoglund <hoglundalva@gmail.com>
2026-02-22 16:09:03 +0100

    Construct effective measurement assuming operator and reservoir are blocks in qn-sectors

--- src/effective_measurements.jl
+++ src/effective_measurements.jl
@@ -1,10 +1,14 @@
-function effective_measurement(op, ρ_reservoir, H_main_qn, H_reservoir, H_total)
-    H_main = hilbert_space(keys(H_main_qn), NumberConservation()) 
-    ρ_reservoir_tot = embed(ρ_reservoir, H_reservoir => H_total; complement = H_main)
-    op_rho = partial_trace(op*ρ_reservoir_tot, H_total => H_main; complement = H_reservoir, alg = FermionicHilbertSpaces.FullPartialTraceAlg())
-    ind = FermionicHilbertSpaces.indices(H_main_qn, H_main)
+function effective_measurement(op_qn :: AbstractMatrix, ψ_res_qn, qd_system)
+    ρ_res_qn = if ψ_res_qn isa AbstractVector
+        ψ_res_qn * ψ_res_qn'
+    else
+        ψ_res_qn
+    end
+    ρ_reservoir = embed(ρ_res_qn, qd_system.H_reservoir_qn, qd_system.H_reservoir)
+    op = embed(op_qn, qd_system.H_total_qn, qd_system.H_total)
+
+    ρ_reservoir_tot = embed(ρ_reservoir, qd_system.H_reservoir => qd_system.H_total; complement = qd_system.H_main)
+    op_rho = partial_trace(op*ρ_reservoir_tot, qd_system.H_total => qd_system.H_main; complement = qd_system.H_reservoir, alg = FermionicHilbertSpaces.FullPartialTraceAlg())
+    ind = FermionicHilbertSpaces.indices(qd_system.H_main_qn, qd_system.H_main)
     return op_rho[ind, ind]
 end
-
-effective_measurement(op, ρ_reservoir, qd_system ::QuantumDotSystem) = 
-    effective_measurement(op, ρ_reservoir, qd_system.H_main_qn, qd_system.H_reservoir,qd_system.H_total)
