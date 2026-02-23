ed8ce869810642c80bceef376d0068da932fcde9
alvahoglund <hoglundalva@gmail.com>
2026-02-23 15:49:22 +0100

    Fix tests for new code structure

--- test/test_time_evolution.jl
+++ test/test_time_evolution.jl
@@ -8,45 +8,57 @@
     op = (rand(ComplexF64, 4, 4) + hc) / 2
     t = 1.0
 
-    ρt = state_time_evolution(ρ, t, ham)
+    ρt = state_time_evolution(t, ρ, ham)
     exp_value_ρ = expectation_value(ρt, op)
 
-    op_t = operator_time_evolution(op, t, ham)
+    op_t = operator_time_evolution(t, op, ham)
     exp_value_op = expectation_value(ρ, op_t)
 
     @test exp_value_op ≈ exp_value_ρ
 end
 @testset "Effective measurements & time evolution" begin
+    using SparseArrays
     ## Initialize system
-    quantum_dot_system = tight_binding_system(2, 3, 1)
+    qd_system = tight_binding_system(2, 3, 1)
     seed = 2
-    hams = hamiltonians(quantum_dot_system, seed)
-    reservoir_state = ground_state(hams.hamiltonian_reservoir, quantum_dot_system.H_reservoir, quantum_dot_system.qn_reservoir)
+    hams = hamiltonians(qd_system, seed)
+    reservoir_state = ground_state(hams.hamiltonian_reservoir, qd_system.H_reservoir_qn)
 
-    initial_states = [def_state(triplet_plus, quantum_dot_system.H_main, quantum_dot_system.f),
-        def_state(singlet, quantum_dot_system.H_main, quantum_dot_system.f),
-        random_product_state(quantum_dot_system),
-        random_separable_state(3, quantum_dot_system)]
-    ρres = reservoir_state * reservoir_state'
-    total_states = map(initial_state -> tensor_product((initial_state, ρres), (quantum_dot_system.H_main, quantum_dot_system.H_reservoir) => quantum_dot_system.H_total), initial_states)
-    measurements = map(op -> matrix_representation(op, quantum_dot_system.H_total), charge_measurements(quantum_dot_system))
-
+    ind = indices(qd_system.H_reservoir_qn, qd_system.H_reservoir)
+    ρ_res = spzeros(ComplexF64, dim(qd_system.H_reservoir), dim(qd_system.H_reservoir))
+    ρ_res[ind, ind] = reservoir_state*reservoir_state'
+    
+    initial_states = [def_state(triplet_plus, qd_system.H_main, qd_system.f),
+        def_state(singlet, qd_system.H_main, qd_system.f),
+        random_product_state(qd_system),
+        random_separable_state(3, qd_system)]
+    
     t = 10
-    ham_total = matrix_representation(hams.hamiltonian_total, quantum_dot_system.H_total)
+    #Time evolve the states in the full hilbert space
+    ham_total = matrix_representation(hams.hamiltonian_total, qd_system.H_total)
+    total_states = map(initial_state -> tensor_product((initial_state, ρ_res), (qd_system.H_main, qd_system.H_reservoir) => qd_system.H_total), initial_states)
+    time_evolved_states = map(total_state -> state_time_evolution(t, total_state, ham_total), total_states)
+    measurements_total = map(op -> matrix_representation(op, qd_system.H_total), charge_measurements(qd_system))
 
-    time_evolved_states = map(total_state -> state_time_evolution(total_state, t, ham_total, quantum_dot_system.H_total, quantum_dot_system.qn_total), total_states)
-    time_evolved_measurements = map(measurement -> operator_time_evolution(measurement, t, ham_total, quantum_dot_system.qn_total, quantum_dot_system.H_total), measurements)
-    effective_measurements = map(measurement -> effective_measurement(measurement, ρres, quantum_dot_system), time_evolved_measurements)
-    scrambling = scrambling_map(quantum_dot_system, t, charge_measurements, QuantumDotReservoir.FullPropagatorAlg(); seed)
+    # Construct effective measurements
+    time_evolved_measurements = map(op -> operator_time_evolution(t, op, hams.hamiltonian_total, qd_system.H_total_qn), charge_measurements(qd_system))
+    effective_measurements = map(measurement -> effective_measurement(measurement, reservoir_state, qd_system), time_evolved_measurements)
 
-    ind = FermionicHilbertSpaces.indices(quantum_dot_system.H_main_qn, quantum_dot_system.H_main)
+    scrambling_block = scrambling_map(qd_system, charge_measurements(qd_system), reservoir_state, hams.hamiltonian_total, t, QRC.BlockPropagatorAlg())
 
-    for i in eachindex(initial_states), j in eachindex(measurements)
-        exp_val_1 = expectation_value(time_evolved_states[i], measurements[j])
-        exp_val_2 = expectation_value(total_states[i], time_evolved_measurements[j])
-        exp_val_3 = expectation_value(initial_states[i][ind, ind], effective_measurements[j])
-        exp_val_4 = (scrambling*vec(initial_states[i][ind, ind]))[j]
-        @test exp_val_1 ≈ exp_val_2 ≈ exp_val_3 ≈ exp_val_4
+    ham_total_qn = matrix_representation(hams.hamiltonian_total, qd_system.H_total_qn)
+    scrambling_pure =  scrambling_map(qd_system, charge_measurements(qd_system), reservoir_state, hams.hamiltonian_total, t, QRC.PureStatePropagatorAlg())
+    
+    ind_tot_qn = FermionicHilbertSpaces.indices(qd_system.H_total_qn, qd_system.H_total)
+    ind_main_qn = FermionicHilbertSpaces.indices(qd_system.H_main_qn, qd_system.H_main)
+
+    for i in eachindex(initial_states), j in eachindex(measurements_total)
+        exp_val_1 = expectation_value(time_evolved_states[i], measurements_total[j])
+        exp_val_2 = expectation_value(total_states[i][ind_tot_qn, ind_tot_qn], time_evolved_measurements[j])
+        exp_val_3 = expectation_value(initial_states[i][ind_main_qn, ind_main_qn], effective_measurements[j])
+        exp_val_4 = (scrambling_block*vec(initial_states[i][ind_main_qn, ind_main_qn]))[j]
+        exp_val_5 = (scrambling_pure*vec(initial_states[i][ind_main_qn, ind_main_qn]))[j]
+        @test exp_val_1 ≈ exp_val_2 ≈ exp_val_3 ≈ exp_val_4 ≈ exp_val_5
     end
 end
 
